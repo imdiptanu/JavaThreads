89e885b677fc393a2f43879c1d97f6cdb19bd3c0
Diptanu Sarkar <diptanusarkar@hotmail.com>
2019-12-31 12:45:41 +0530

    Removed anomalies

--- EclipseJavaHandsOn/src/threads_durgasoft/ChildThread.java
+++
@@ -1,9 +0,0 @@
-package threads_durgasoft;
-
-public class ChildThread extends Thread {
-	public void run() {
-		for (int index = 0; index < 10; index++) {
-			System.out.println("Child Thread");
-		}
-	}
-}
